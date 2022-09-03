a6b4843058b6251d6d17112574a0971ff9838696
Joan Gavelán <joangavelan@gmail.com>
2022-09-02 19:46:46 -0500

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Component from '@/components/Component'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <Component />
+    </div>
+  )
+}
+
+export default App
