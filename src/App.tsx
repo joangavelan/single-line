de3b4379a9ecc4ddf0d136bdbc9ba1f73c63ad1d
Joan Gavelán <joangavelan@gmail.com>
2022-09-03 20:31:10 -0500

    configure eslint, prettier and husky

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Component from '@/components/Component'
 const App = () => {
   return (
     <div>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Component />
     </div>
   )
